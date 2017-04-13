7a7d75b2f7861dcc8758b01f16f67a07cc73d7c9
zblash <zhangbaolong@T4F-MAM-13092.local>
2017-04-13 17:54:48 +0800

    the first commit

---
+++ test.py
@@ -0,0 +1,21 @@
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
+DATABASE = '/tmp/flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+
+app = Flask(__name__)
+app.config.from_envvar('VENV_SETTINGS',silent=True)
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+    
+if __name__ == '__main__':
+    app.run()
